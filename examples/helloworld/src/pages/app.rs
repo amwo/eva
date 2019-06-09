172891ecbbc61e5455d7cca8fb58a5279d4fe5f4
amwo <nuude.ad@gmail.com>
2019-06-09 22:37:29 +0900

    Change component architecture.

--- examples/helloworld/src/pages/app.rs
+++ examples/helloworld/src/pages/app.rs
@@ -1,41 +1,22 @@
 use eva::{html, Component, Html, Renderable};
 
-pub struct Root;
+struct Root {}
 
-pub enum Action {
-    Increment,
-    Decrement,
-    Bulk(Vec<Msg>),
+impl Component for Root {}
+
+impl CSS for Root {
+    css!({
+        .title {
+            font-size: 33px;
+        }
+    });
 }
 
-impl Component for App {}
-
 impl Render<Root> for Root {
-    fn view(&self) -> Html<Self> {
-        if states.login == 0 {
-            html! (#"
-            <div>
-                <nav class="menu",>
-                    <button onclick=|_| Msg::Increment,>{ "Increment" }</button>
-                    <button onclick=|_| Msg::Decrement,>{ "Decrement" }</button>
-                    <button onclick=|_| Msg::Bulk(vec![Msg::Increment, Msg::Increment]),>{ "Increment Twice" }</button>
-                </nav>
-                <p>{ self.value }</p>
-                <p>{ Date::new().to_string() }</p>
-            </div>
-            "#);
-        } else {
-            html! (#"
-            <div>
-                <nav class="menu",>
-                    <button onclick=|_| Msg::Increment,>{ "Increment" }</button>
-                    <button onclick=|_| Msg::Decrement,>{ "Decrement" }</button>
-                    <button onclick=|_| Msg::Bulk(vec![Msg::Increment, Msg::Increment]),>{ "Increment Twice" }</button>
-                </nav>
-                <p>{ self.value }</p>
-                <p>{ Date::new().to_string() }</p>
-            </div>
-            "#);
-        }
-    }
+    render!({
+        html!({
+            <h1 class="title">Hello!</h1>
+            <button onclick="increment"></button>
+        });
+    });
 }
