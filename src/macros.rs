e67238fa6cf8154acc53ddc6400faa343fb7eca4
amwo <nuude.ad@gmail.com>
2019-06-09 22:36:49 +0900

    Change html! macro flugment raw string to token tree.

--- src/macros.rs
+++ src/macros.rs
@@ -1,12 +1,26 @@
 #[macro_export]
 macro_rules! html {
-    ($rh:expr) => {
-        println!("{}", $rh)
+    ($t:tt) => {
+        println!("tt: {}", stringify!($t))
     };
 }
 
 #[macro_export]
 macro_rules! render {
+    ($html:expr) => {
+        println!("{}", $html);
+    };
+}
+
+#[macro_export]
+macro_rules! css {
+    ($html:expr) => {
+        println!("{}", $html);
+    };
+}
+
+#[macro_export]
+macro_rules! js {
     ($html:expr) => {
         println!("{}", $html);
     };
